60777cd940da746c3b8221d76a64d93aa80c352e
TymurBondar <tymurbondar@outlook.com>
2023-10-10 12:07:17 -0400

    more button almost working

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi";
 
-function SushiContainer({sushis, eatSushi, eaten}) {
+function SushiContainer({ sushis, eatSushi, eaten }) {
+
+  const [startIndex, setStartIndex] = useState(0);
+
+  // Function to update startIndex when "More Sushis" button is clicked
+  const moreSushi = () => {
+    // Update startIndex to display the next 4 sushis
+    setStartIndex(startIndex + 4);
+    if (startIndex + 4 > sushis.length) {
+      setStartIndex(0);
+    };
+  };
+
+  // Get the 4 sushis to display
+  const displayedSushis = sushis.slice(startIndex, startIndex + 4);
+
   return (
     <div className="belt">
-      {sushis.map( sushi => <Sushi key={sushi.id} sushi={sushi} eatSushi={eatSushi} eaten={eaten}/>)}
-      <MoreButton />
+      {displayedSushis.map(sushi => <Sushi key={sushi.id} sushi={sushi} eatSushi={eatSushi} eaten={eaten} />)}
+      <MoreButton moreSushi={moreSushi} />
     </div>
   );
 }
